ec8b617192866533513fd854a612434f6752a456
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-11-08 21:19:37 +0530

    working on home page

---
+++ frontend/src/Components/Home/home.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import Categories from '../Layouts/Categories';
+import Banner from './Banner/Banner';
+import DealSlider from './DealSlider/DealSlider';
+import ProductSlider from './ProductSlider/ProductSlider';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearErrors, getSliderProducts } from '../../actions/productAction';
+import { useSnackbar } from 'notistack';
+import MetaData from '../Layouts/MetaData';
+
+const Home = () =>{
+    
+}
