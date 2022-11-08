e16b0acb5015eb3c7426070dec366050c6ed3ba7
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-11-09 00:27:04 +0530

    done banner.jsx and banner.css

--- frontend/src/Components/Home/Banner/Banner.jsx
+++ frontend/src/Components/Home/Banner/Banner.jsx
@@ -18,4 +18,44 @@ return (
         <ArrowBackIosIcon />
     </div>
 )
-}
+};
+
+export const NextBtn =({ className , onClick }) =>{
+    return (
+        <div className={className} onClick={onClick}>
+            <ArrowForwardIosIcon />
+        </div>
+    )
+};
+
+const Banner = () =>{
+    const settings ={
+        autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PreviousBtn />,
+    nextArrow: <NextBtn />,
+    };
+
+    const banners = [gadgetSale , kitchenSale , poco , fashionSale , realme , oppo];
+
+    return (
+        <>
+            <section className='h-44 sm:h-72 w-full rounded-sm shadow relative overflow-hidden'>
+                <Slider {...settings}>
+                {banners.map((el, i) => (
+            <img draggable="false" className="h-44 sm:h-72 w-full object-cover" src={el} alt="banner" key={i}/>
+          ))};
+                </Slider>
+            </section>
+        </>
+    )
+
+
+};
+
+export default Banner;
