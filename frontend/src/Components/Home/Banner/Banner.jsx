8a388364ba401c610d027c5dfd6b00348af110a7
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-11-09 00:15:07 +0530

    woking on appjs indexjs and now on banner

---
+++ frontend/src/Components/Home/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Banner.css';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import gadgetSale from '../../../assets/images/Banners/gadget-sale.jpg';
+import kitchenSale from '../../../assets/images/Banners/kitchen-sale.jpg';
+import poco from '../../../assets/images/Banners/poco-m4-pro.webp';
+import realme from '../../../assets/images/Banners/realme-9-pro.webp';
+import fashionSale from '../../../assets/images/Banners/fashionsale.jpg';
+import oppo from '../../../assets/images/Banners/oppo-reno7.webp';
+
+
+export const PreviousBtn =({ className , onClick})=>{
+return (
+    <div className={className} onClick={onClick}>
+        <ArrowBackIosIcon />
+    </div>
+)
+}
