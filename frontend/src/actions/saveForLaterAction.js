42ee8c0aaa692634a221228436f6cd8952d445c1
arpitmiahra4 <arpitmiahra4@gmail.com>
2022-11-09 16:08:04 +0530

    Product/product.jsx complete

--- frontend/src/actions/saveForLaterAction.js
+++ frontend/src/actions/saveForLaterAction.js
@@ -1,26 +1,33 @@
-import { REMOVE_FROM_SAVE_FOR_LATER, SAVE_FOR_LATER } from "../constants/saveForLaterConstants";
+import {
+  REMOVE_FROM_SAVE_FOR_LATER,
+  SAVE_FOR_LATER,
+} from "../constants/saveForLaterConstants";
 
 // Save For Later
 export const saveForLater = (id) => async (dispatch, getState) => {
+  const cartItemsArr = getState().cart.cartItems;
+  const product = cartItemsArr.find((i) => i.product === id);
 
-    const cartItemsArr = getState().cart.cartItems;
-    const product = cartItemsArr.find((i) => i.product === id)
+  dispatch({
+    type: SAVE_FOR_LATER,
+    payload: product,
+  });
 
-    dispatch({
-        type: SAVE_FOR_LATER,
-        payload: product
-    });
-
-    localStorage.setItem('saveForLaterItems', JSON.stringify(getState().saveForLater.saveForLaterItems))
-}
+  localStorage.setItem(
+    "saveForLaterItems",
+    JSON.stringify(getState().saveForLater.saveForLaterItems)
+  );
+};
 
 // Remove From Save For Later
 export const removeFromSaveForLater = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: REMOVE_FROM_SAVE_FOR_LATER,
+    payload: id,
+  });
 
-    dispatch({
-        type: REMOVE_FROM_SAVE_FOR_LATER,
-        payload: id,
-    });
-
-    localStorage.setItem('saveForLaterItems', JSON.stringify(getState().saveForLater.saveForLaterItems))
-}
+  localStorage.setItem(
+    "saveForLaterItems",
+    JSON.stringify(getState().saveForLater.saveForLaterItems)
+  );
+};
